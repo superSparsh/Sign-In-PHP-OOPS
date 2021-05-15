d0ad080e0593ff060a4d87456f419da6f3136b27
Sparsh Thakur <82032891+superSparsh@users.noreply.github.com>
2021-05-15 18:03:45 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -12,13 +12,16 @@ class signin{
 			echo "Failed to connect to Mysql:".$this->mysql->connect_error;
 		}
 	}
+	function error(){
+		echo "Something went wrong" .$this->mysqli->connect_error;
+	}
 	function tableExists($tableName){
 			$sql = "SHOW TABLES FROM $this->dbname LIKE '$tableName'";
 			$run = $this->mysqli->query($sql);
 			if($run->num_rows == 1){
 				echo "Table Exists";
 			}else{
-				echo $this->mysqli->error;
+				echo $this->error();
 			}
 		}
 	function userAvailabilty($tableName,$email){
@@ -27,7 +30,7 @@ class signin{
 		if($run->num_rows >= 1){
 			return $run;
 		}else{
-			echo $this->mysqli->error;
+			echo $this->error();
 		}	
 	}
 	function sign($tableName,$fname,$lname,$email,$password){
@@ -36,7 +39,7 @@ class signin{
 			$run = $this->mysqli->query($sql);
 			return $run;
 		}else{
-			echo $this->mysqli->error; 
+			echo $this->error(); 
 		}
 	}
 }
